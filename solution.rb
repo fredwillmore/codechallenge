12d1c4f8d31f2530566cb8663e2365c4ecc3b1aa
Fred Willmore <fredwillmore@gmail.com>
2014-04-04 12:34:56 -0700

    Made a significant improvement in search time with improved search structure. We incur a bit of overhead by creating 26*26*26 arrays for storing previously counted sequences but that is insignificant compared with the efficiency gained in searching

--- solution.rb
+++ solution.rb
@@ -39,33 +39,75 @@ class Sequence
 end
 
 class SequenceSearch
+  NORMALIZE=97
+  LETTERS=26
+  
   def initialize
     @structure=[]
+    # divide the search space into 26*26*26 reasonably small partitions which can be quickly found and searched
+    LETTERS.times do |i|
+      @structure[i] = []
+      LETTERS.times do |j|
+        @structure[i][j] = []
+        LETTERS.times do |k|
+          @structure[i][j][k] = []        
+        end
+      end
+    end
   end
   
   def find_or_add(s)
-    index = @structure.find_index do |item| 
+    # derive nested array indices from first character of each word of the sequence
+    i = s.word1[0].ord-NORMALIZE
+    j = s.word2[0].ord-NORMALIZE
+    k = s.word3[0].ord-NORMALIZE
+
+    # search to see if the sequence has already been counted
+    index = @structure[i][j][k].find_index do |item| 
       item.match s
     end
 
+    # if the sequence has not been counted yet, add it to the appropriate partition
+    # if it has, increment its count
     if index.nil?
-      @structure << s
+      @structure[i][j][k] << s
     else
-      @structure[index].increment_count
+      @structure[i][j][k][index].increment_count
     end
   end
 
   def show_top
-    @structure.sort! { |a,b| b.count <=> a.count }
-    @structure.slice(0,100).each do |s|
+    @structure.flatten!.sort! { |a,b| b.count <=> a.count }.slice(0,100).each do |s|
       puts s.readable 
+    end
+    i=100
+    # the following values are tied for last place
+    while !@structure[i].nil? && @structure[i].count==@structure[99].count
+      puts @structure[i].readable 
+      i+=1
+    end
+  end
+  
+  def inspect
+    @structure.each do |l1|
+      l1.each do |l2|
+        l2.each do |l3|
+          l3.each do |s|
+            puts s.readable
+          end
+        end
+      end
     end
   end
 end
   
+time1 = Time.now.to_f; # simple profiling info
 
+# read the file(s) or stream
 words = ARGF.read.gsub(/[^a-zA-Z\s]/m, '').downcase.split(/\s+/)
-exit if words.length < 3
+exit if words.length < 3 
+
+time2 = Time.now.to_f; # simple profiling info
 
 sequence_search = SequenceSearch.new
 
@@ -79,5 +121,12 @@ words.each do |word3|
   word2=word3
 end
 
+time3 = Time.now.to_f; # simple profiling info
+
 sequence_search.show_top
 
+time4 = Time.now.to_f;  # simple profiling info
+
+puts "File Read Time: #{time2-time1}"
+puts "Process Time: #{time3-time2}"
+puts "Sort Time: #{time4-time3}"
